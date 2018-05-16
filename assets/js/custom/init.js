b123a0120f84517acb9cd6a459a92bd1db217708
Atul <2012ucp1430@mnit.ac.in>
2018-05-16 15:54:15 +0530

    Adding cards

---
+++ assets/js/custom/init.js
@@ -0,0 +1,68 @@
+/**
+ * @file
+ * Handles initialization events and declaration of global variables
+ */
+
+//Global Variables
+var phpSDKMetaData;
+var javaSDKMetaData;
+var jsSDKMetaData;
+
+
+/**
+ * Control starts here. (Document.ready)
+ */
+$(function(){
+    loadJS();
+});
+
+function loadJS() {
+    $.getJSON('https://api.github.com/repos/2012ucp1430/lp2/commits', function(data) {//Get Config From File
+        loadConfigJS(data);
+    });
+}
+
+function loadConfigJS(json){
+    if (typeof json!="string") {
+        jsSDKMetaData=json;
+    } else {
+        jsSDKMetaData=JSON.parse(json);
+    }
+    loadPHP();
+}
+
+function loadPHP() {
+    $.getJSON('https://api.github.com/repos/2012ucp1430/lp1/commits', function(data) {//Get Config From File
+        loadConfigPHP(data);
+    });
+}
+
+function loadConfigPHP(json){
+    if (typeof json!="string") {
+        phpSDKMetaData=json;
+    } else {
+        phpSDKMetaData=JSON.parse(json);
+    }
+    loadJava();
+}
+
+function loadJava() {
+    $.getJSON('https://api.github.com/repos/2012ucp1430/landing.page/commits', function(data) {//Get Config From File
+        loadConfigJava(data);
+    });
+}
+function loadConfigJava(json){
+    if (typeof json!="string") {
+        javaSDKMetaData=json;
+    } else {
+        javaSDKMetaData=JSON.parse(json);
+    }
+    init();
+}
+
+/**
+ * Function that initializes execution - handlers, requests, UI
+ */
+function init(){
+    console.log("init starts");
+}
